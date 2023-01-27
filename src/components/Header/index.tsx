bab7815192e04ba4c1399efcefa586ff8e8bb90c
Filipe Augusto <filipeaugusto2130@gmail.com>
2023-01-27 20:56:51 -0300

    Criando o Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,17 @@
 import logo from 'assets/logo_cloud.png';
+import styles from './Header.module.scss';
 
 export default function Header() {
   return (
     <header>
-      <h1><img src={logo} alt="Logo do site" /></h1>      
+      <nav className={styles.navbar}>
+        <h1 className={styles.navbar__logo}><img src={logo} alt="Logo do site" /></h1>    
+        <ul className={styles.navbar__list}>
+          <li>Cadastrar Carro</li>
+          <li>Entrada</li>
+          <li>Saída</li>
+        </ul>  
+      </nav>    
     </header>
   );
 }
