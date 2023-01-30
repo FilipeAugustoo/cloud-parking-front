87eb11c4eb1a2348863726d785ebb617ddcc6f8b
Filipe Augusto <filipeaugusto2130@gmail.com>
2023-01-30 20:56:28 -0300

    Criando a Home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,122 @@
-import Header from "../../components/Header";
+import styles from './Home.module.scss';
 
 export default function Home() {
   return (
-    <Header />
+    <main className={styles.main}>
+      <section className={styles.box}>
+        <div className={styles.box__item}>
+          <h2>Modelo: Corsa</h2>
+          <p>Placa: HFB-7364</p>
+          <p>Cor: VERMELHO</p>
+          <p>Estado: DF</p>
+          <p>Entrada: 15:00 20/12/2022</p>
+        </div>
+        <div className={styles.box__item}>
+          <h2>Modelo: Corsa</h2>
+          <p>Placa: HFB-7364</p>
+          <p>Cor: VERMELHO</p>
+          <p>Estado: DF</p>
+          <p>Entrada: 15:00 20/12/2022</p>
+        </div>
+        <div className={styles.box__item}>
+          <h2>Modelo: Corsa</h2>
+          <p>Placa: HFB-7364</p>
+          <p>Cor: VERMELHO</p>
+          <p>Estado: DF</p>
+          <p>Entrada: 15:00 20/12/2022</p>
+        </div>
+        <div className={styles.box__item}>
+          <h2>Modelo: Corsa</h2>
+          <p>Placa: HFB-7364</p>
+          <p>Cor: VERMELHO</p>
+          <p>Estado: DF</p>
+          <p>Entrada: 15:00 20/12/2022</p>
+        </div>
+        <div className={styles.box__item}>
+          <h2>Modelo: Corsa</h2>
+          <p>Placa: HFB-7364</p>
+          <p>Cor: VERMELHO</p>
+          <p>Estado: DF</p>
+          <p>Entrada: 15:00 20/12/2022</p>
+        </div>
+        <div className={styles.box__item}>
+          <h2>Modelo: Corsa</h2>
+          <p>Placa: HFB-7364</p>
+          <p>Cor: VERMELHO</p>
+          <p>Estado: DF</p>
+          <p>Entrada: 15:00 20/12/2022</p>
+        </div>
+        <div className={styles.box__item}>
+          <h2>Modelo: Corsa</h2>
+          <p>Placa: HFB-7364</p>
+          <p>Cor: VERMELHO</p>
+          <p>Estado: DF</p>
+          <p>Entrada: 15:00 20/12/2022</p>
+        </div>
+        <div className={styles.box__item}>
+          <h2>Modelo: Corsa</h2>
+          <p>Placa: HFB-7364</p>
+          <p>Cor: VERMELHO</p>
+          <p>Estado: DF</p>
+          <p>Entrada: 15:00 20/12/2022</p>
+        </div>
+        <div className={styles.box__item}>
+          <h2>Modelo: Corsa</h2>
+          <p>Placa: HFB-7364</p>
+          <p>Cor: VERMELHO</p>
+          <p>Estado: DF</p>
+          <p>Entrada: 15:00 20/12/2022</p>
+        </div>
+        <div className={styles.box__item}>
+          <h2>Modelo: Corsa</h2>
+          <p>Placa: HFB-7364</p>
+          <p>Cor: VERMELHO</p>
+          <p>Estado: DF</p>
+          <p>Entrada: 15:00 20/12/2022</p>
+        </div>
+        <div className={styles.box__item}>
+          <h2>Modelo: Corsa</h2>
+          <p>Placa: HFB-7364</p>
+          <p>Cor: VERMELHO</p>
+          <p>Estado: DF</p>
+          <p>Entrada: 15:00 20/12/2022</p>
+        </div>
+        <div className={styles.box__item}>
+          <h2>Modelo: Corsa</h2>
+          <p>Placa: HFB-7364</p>
+          <p>Cor: VERMELHO</p>
+          <p>Estado: DF</p>
+          <p>Entrada: 15:00 20/12/2022</p>
+        </div>
+        <div className={styles.box__item}>
+          <h2>Modelo: Corsa</h2>
+          <p>Placa: HFB-7364</p>
+          <p>Cor: VERMELHO</p>
+          <p>Estado: DF</p>
+          <p>Entrada: 15:00 20/12/2022</p>
+        </div>
+        <div className={styles.box__item}>
+          <h2>Modelo: Corsa</h2>
+          <p>Placa: HFB-7364</p>
+          <p>Cor: VERMELHO</p>
+          <p>Estado: DF</p>
+          <p>Entrada: 15:00 20/12/2022</p>
+        </div>
+        <div className={styles.box__item}>
+          <h2>Modelo: Corsa</h2>
+          <p>Placa: HFB-7364</p>
+          <p>Cor: VERMELHO</p>
+          <p>Estado: DF</p>
+          <p>Entrada: 15:00 20/12/2022</p>
+        </div>
+        <div className={styles.box__item}>
+          <h2>Modelo: Corsa</h2>
+          <p>Placa: HFB-7364</p>
+          <p>Cor: VERMELHO</p>
+          <p>Estado: DF</p>
+          <p>Entrada: 15:00 20/12/2022</p>
+        </div>
+      </section>
+    </main>
   );
 }
