40919d8bab123b2bcd3aec0b33c55a533beb6ccf
Filipe Augusto <filipeaugusto2130@gmail.com>
2023-02-01 21:32:11 -0300

    Terminando o css da home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,121 +1,22 @@
+import { useState } from 'react';
+import carsJson from './data.json';
 import styles from './Home.module.scss';
 
 export default function Home() {
+  const [cars, setCars] = useState(carsJson);
+
   return (
     <main className={styles.main}>
       <section className={styles.box}>
+       {cars.cars.map(car => (
         <div className={styles.box__item}>
-          <h2>Modelo: Corsa</h2>
-          <p>Placa: HFB-7364</p>
-          <p>Cor: VERMELHO</p>
-          <p>Estado: DF</p>
-          <p>Entrada: 15:00 20/12/2022</p>
+          <h2>Modelo: {car.model}</h2>
+          <p>Placa: {car.license}</p>
+          <p>Cor: {car.color}</p>
+          <p>Estado: {car.state}</p>
+          <p>Entrada: {car.entryDate}</p>
         </div>
-        <div className={styles.box__item}>
-          <h2>Modelo: Corsa</h2>
-          <p>Placa: HFB-7364</p>
-          <p>Cor: VERMELHO</p>
-          <p>Estado: DF</p>
-          <p>Entrada: 15:00 20/12/2022</p>
-        </div>
-        <div className={styles.box__item}>
-          <h2>Modelo: Corsa</h2>
-          <p>Placa: HFB-7364</p>
-          <p>Cor: VERMELHO</p>
-          <p>Estado: DF</p>
-          <p>Entrada: 15:00 20/12/2022</p>
-        </div>
-        <div className={styles.box__item}>
-          <h2>Modelo: Corsa</h2>
-          <p>Placa: HFB-7364</p>
-          <p>Cor: VERMELHO</p>
-          <p>Estado: DF</p>
-          <p>Entrada: 15:00 20/12/2022</p>
-        </div>
-        <div className={styles.box__item}>
-          <h2>Modelo: Corsa</h2>
-          <p>Placa: HFB-7364</p>
-          <p>Cor: VERMELHO</p>
-          <p>Estado: DF</p>
-          <p>Entrada: 15:00 20/12/2022</p>
-        </div>
-        <div className={styles.box__item}>
-          <h2>Modelo: Corsa</h2>
-          <p>Placa: HFB-7364</p>
-          <p>Cor: VERMELHO</p>
-          <p>Estado: DF</p>
-          <p>Entrada: 15:00 20/12/2022</p>
-        </div>
-        <div className={styles.box__item}>
-          <h2>Modelo: Corsa</h2>
-          <p>Placa: HFB-7364</p>
-          <p>Cor: VERMELHO</p>
-          <p>Estado: DF</p>
-          <p>Entrada: 15:00 20/12/2022</p>
-        </div>
-        <div className={styles.box__item}>
-          <h2>Modelo: Corsa</h2>
-          <p>Placa: HFB-7364</p>
-          <p>Cor: VERMELHO</p>
-          <p>Estado: DF</p>
-          <p>Entrada: 15:00 20/12/2022</p>
-        </div>
-        <div className={styles.box__item}>
-          <h2>Modelo: Corsa</h2>
-          <p>Placa: HFB-7364</p>
-          <p>Cor: VERMELHO</p>
-          <p>Estado: DF</p>
-          <p>Entrada: 15:00 20/12/2022</p>
-        </div>
-        <div className={styles.box__item}>
-          <h2>Modelo: Corsa</h2>
-          <p>Placa: HFB-7364</p>
-          <p>Cor: VERMELHO</p>
-          <p>Estado: DF</p>
-          <p>Entrada: 15:00 20/12/2022</p>
-        </div>
-        <div className={styles.box__item}>
-          <h2>Modelo: Corsa</h2>
-          <p>Placa: HFB-7364</p>
-          <p>Cor: VERMELHO</p>
-          <p>Estado: DF</p>
-          <p>Entrada: 15:00 20/12/2022</p>
-        </div>
-        <div className={styles.box__item}>
-          <h2>Modelo: Corsa</h2>
-          <p>Placa: HFB-7364</p>
-          <p>Cor: VERMELHO</p>
-          <p>Estado: DF</p>
-          <p>Entrada: 15:00 20/12/2022</p>
-        </div>
-        <div className={styles.box__item}>
-          <h2>Modelo: Corsa</h2>
-          <p>Placa: HFB-7364</p>
-          <p>Cor: VERMELHO</p>
-          <p>Estado: DF</p>
-          <p>Entrada: 15:00 20/12/2022</p>
-        </div>
-        <div className={styles.box__item}>
-          <h2>Modelo: Corsa</h2>
-          <p>Placa: HFB-7364</p>
-          <p>Cor: VERMELHO</p>
-          <p>Estado: DF</p>
-          <p>Entrada: 15:00 20/12/2022</p>
-        </div>
-        <div className={styles.box__item}>
-          <h2>Modelo: Corsa</h2>
-          <p>Placa: HFB-7364</p>
-          <p>Cor: VERMELHO</p>
-          <p>Estado: DF</p>
-          <p>Entrada: 15:00 20/12/2022</p>
-        </div>
-        <div className={styles.box__item}>
-          <h2>Modelo: Corsa</h2>
-          <p>Placa: HFB-7364</p>
-          <p>Cor: VERMELHO</p>
-          <p>Estado: DF</p>
-          <p>Entrada: 15:00 20/12/2022</p>
-        </div>
+       ))}
       </section>
     </main>
   );
