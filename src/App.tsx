3fa7bfe38f8df7f23e1e88962508534103d70d83
Filipe Augusto <filipeaugusto2130@gmail.com>
2023-02-03 21:35:48 -0300

    Adicionando as rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
-import Header from "components/Header";
-import Home from "./pages/Home";
+import Rotas from "Routes";
+
 
 function App() {
   return (
-    <>
-      <Header />
-      <Home />
-    </>
+    <Rotas />
   );
 }
 
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,15 +1,24 @@
 import logo from 'assets/logo_cloud.png';
+import { NavLink } from 'react-router-dom';
 import styles from './Header.module.scss';
 
 export default function Header() {
   return (
     <header>
       <nav className={styles.navbar}>
-        <h1 className={styles.navbar__logo}><img src={logo} alt="Logo do site" /></h1>    
+        <NavLink to="/home" className={styles.navbar__logo}><img src={logo} alt="Logo do site" /></NavLink>    
         <ul className={styles.navbar__list}>          
-          <li>Registrar Entrada</li>
-          <li>Cadastrar Carro</li>
-          <li>Lista De Carros</li>
+          <NavLink className={styles.navbar__list__item} to="" style={({ isActive }) =>
+            isActive ? { color: 'white' } : undefined
+          }>Registrar Entrada</NavLink>
+
+          <NavLink className={styles.navbar__list__item} to="/cadastrar_carro" style={({ isActive }) =>
+            isActive ? { color: 'white' } : undefined
+          }>Cadastrar Carro</NavLink>
+          
+          <NavLink className={styles.navbar__list__item} to="" style={({ isActive }) =>
+            isActive ? { color: 'white' } : undefined
+          }>Lista De Carros</NavLink>
         </ul>  
       </nav>    
     </header>
