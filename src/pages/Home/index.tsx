f238d6df73ca7d3e7d4e0819ce2a92798b4d6f78
Filipe Augusto <filipeaugusto2130@gmail.com>
2023-02-04 20:54:24 -0300

    Adicionando o component Box

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,3 +1,4 @@
+import Box from 'components/Box';
 import { useEffect, useState } from 'react';
 import carsJson from './data.json';
 import styles from './Home.module.scss';
@@ -16,22 +17,19 @@ export default function Home() {
   }
 
   return (
-    <main className={styles.main}>
-      <section className={styles.box}>
-        <div className={styles.box__itens}>
-          {cars.cars.map(car => (
-            <div className={styles.box__item} onClick={() => {setCar(car.model); setLicense(car.license)}}>
-              <h2>Modelo: {car.model}</h2>
-              <p>Placa: {car.license}</p>
-              <p>Cor: {car.color}</p>
-              <p>Estado: {car.state}</p>
-              <p>Entrada: {car.entryDate}</p>
-            </div>
-          ))}
-        </div>
-        <button className={styles.box__botao} onClick={() => teste(license)}>Registrar Saída: {car}</button>
-      </section>
-      
-    </main>
+    <Box>
+      <div className={styles.itens}>
+        {cars.cars.map(car => (
+          <div className={styles.item} onClick={() => {setCar(car.model); setLicense(car.license)}}>
+            <h2>Modelo: {car.model}</h2>
+            <p>Placa: {car.license}</p>
+            <p>Cor: {car.color}</p>
+            <p>Estado: {car.state}</p>
+            <p>Entrada: {car.entryDate}</p>
+          </div>
+        ))}
+      </div>
+      <button className={styles.botao} onClick={() => teste(license)}>Registrar Saída: {car}</button>
+    </Box>
   );
 }
