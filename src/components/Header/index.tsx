e6e139032293fbee14ad451c30e5b22e1850fc31
Filipe Augusto <filipeaugusto2130@gmail.com>
2023-02-10 21:34:39 -0300

    implementando a logica de login no header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,23 @@
 import logo from 'assets/logo_cloud.png';
+import classNames from 'classnames';
+import { AuthContext } from 'contexts/auth/AuthContext';
+import { useContext } from 'react';
 import { NavLink } from 'react-router-dom';
 import styles from './Header.module.scss';
 
 export default function Header() {
+  const auth = useContext(AuthContext);
+  const user = auth.user;
+
   return (
     <header>
       <nav className={styles.navbar}>
-        <NavLink to="/" className={styles.navbar__logo}><img src={logo} alt="Logo do site" /></NavLink>    
-        <ul className={styles.navbar__list}>          
+        <NavLink to="/" className={classNames({
+          [styles.navbar__logo]: user,
+          [styles.navbar__logo__center]: !user
+        })}><img src={logo} alt="Logo do site" /></NavLink>   
+
+        {user ? <ul className={styles.navbar__list}>          
           <NavLink className={styles.navbar__list__item} to="" style={({ isActive }) =>
             isActive ? { color: 'white' } : undefined
           }>Registrar Entrada</NavLink>
@@ -19,7 +29,8 @@ export default function Header() {
           <NavLink className={styles.navbar__list__item} to="" style={({ isActive }) =>
             isActive ? { color: 'white' } : undefined
           }>Lista De Carros</NavLink>
-        </ul>  
+        </ul>  : ''}
+        
       </nav>    
     </header>
   );
