cb801aef88c4e4fbb9ddeafa3c8b39d6f445ea99
Filipe Augusto <filipeaugusto2130@gmail.com>
2023-03-08 22:25:27 -0300

    Finalizando a page de Cadastrar Carro

--- src/pages/CadastrarCarro/index.tsx
+++ src/pages/CadastrarCarro/index.tsx
@@ -1,37 +1,45 @@
 import Box from "components/Box";
+import { useApi } from "hooks/useApi";
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { CarCreate } from "types/CarCreate";
 import styles from './CadastrarCarro.module.scss';
 
 export default function CadastrarCarro() {
 
-  const [modelo, setModelo] = useState('');
-  const [cor, setCor] = useState('');
-  const [placa, setPlaca] = useState('');
-  const [estado, setEstado] = useState('');
+  const [car, setCar] = useState<CarCreate>({
+    color: "",
+    license: "",
+    model: "",
+    state: ""
+  });
 
-  function testeForm(e: React.FormEvent<HTMLFormElement>) {
+  const api = useApi();
+  const navigate = useNavigate();
+
+  function createCar(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault();
+    api.createCar(car);
 
-    const carro = {
-      model: modelo,
-      color: cor,
-      license: placa,
-      state: estado
-    }
-
-    console.log(carro)
+    navigate('/');
   }
 
   return (
     <Box>
-      <form className={styles.form} onSubmit={testeForm}>
+      <form className={styles.form} onSubmit={createCar}>
         <input 
           className={styles.form__input} 
           type="text" 
           name="modelo_carro" 
           id="modelo_carro" 
           placeholder="Modelo Do Carro"
-          onChange={(e) => setModelo(e.target.value)}
+          value={car.model}
+          onChange={(event) => {
+            setCar({
+              ...car,
+              model: event.currentTarget?.value || "",
+            })
+          }}
         />
 
         <input 
@@ -40,7 +48,13 @@ export default function CadastrarCarro() {
           name="cor_carro" 
           id="cor_carro" 
           placeholder="Cor Do Carro"
-          onChange={(e) => setCor(e.target.value)}
+          value={car.color}
+          onChange={(event) => {
+            setCar({
+              ...car,
+              color: event.currentTarget?.value || "",
+            })
+          }}
         />
 
         <input 
@@ -49,7 +63,13 @@ export default function CadastrarCarro() {
           name="placa_carro" 
           id="placa_carro" 
           placeholder="Placa Do Carro"
-          onChange={(e) => setPlaca(e.target.value)} 
+          value={car.license}
+          onChange={(event) => {
+            setCar({
+              ...car,
+              license: event.currentTarget?.value || "",
+            })
+          }}
         />
 
         <input 
@@ -58,7 +78,13 @@ export default function CadastrarCarro() {
           name="estado_carro" 
           id="estado_carro" 
           placeholder="Estado Do Carro"
-          onChange={(e) => setEstado(e.target.value)} 
+          value={car.state}
+          onChange={(event) => {
+            setCar({
+              ...car,
+              state: event.currentTarget?.value || "",
+            })
+          }}
         />
 
         <button className={styles.form__button}>Cadastrar</button>
